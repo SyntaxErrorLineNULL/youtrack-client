41d8cf98544167f0fc7f09d01fa0c8cb43778025
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2022-06-09 22:53:17 +0300

    create config

---
+++ config.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+type YouTrack struct {
+	URL   string
+	Token string
+}
+
+func loadConfig() (*YouTrack, error) {
+	config := &YouTrack{}
+	for _, env := range []string{"YOUTRACK_URL", "YOUTRACK_TOKEN"} {
+		value := os.Getenv(env)
+		if value == "" {
+			return config, fmt.Errorf("empty value for %s", env)
+		}
+		switch env {
+		case "YOUTRACK_URL":
+			config.URL = env
+		case "YOUTRACK_TOKEN":
+			config.Token = env
+
+		}
+	}
+
+	return config, nil
+}
