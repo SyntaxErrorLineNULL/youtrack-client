60cf097ee6d73bcb78d4ffa733e3f4b2158197e5
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2022-12-24 22:22:54 +0300

    error api fields

---
+++ error.go
@@ -0,0 +1,18 @@
+package main
+
+// IncorrectField
+// This article examines an error that might happen when you try to update
+// a custom field in an issue using YouTrack REST API.
+// You can face this error while making requests to different endpoints.
+// This topic describes the most common scenario when you can receive this error message.
+type IncorrectField struct {
+	CustomFields []Fields `json:"customFields"`
+}
+
+type Fields struct {
+	Name  string `json:"name,omitempty"`
+	Type  string `json:"$type,omitempty"`
+	Value struct {
+		Name string `json:"name,omitempty"`
+	}
+}
